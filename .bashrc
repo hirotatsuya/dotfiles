8aa3620a3420aa079022d307f26efffb1568c231
hirotatsu <tatsuya_hirota@kronos.jp>
2018-06-25 09:22:55 +0900

    add inc dotsfile

--- .bashrc
+++ .bashrc
@@ -1,45 +1,14 @@
-# Tatsuya's bash settings
-# alias
-alias f='fish'
-alias e='exit'
-alias l='ll'
-alias ll='ls -l'
-alias lll='ll'
-alias la='ls -la'
-alias sbp='source $HOME/.bash_profile'
-alias bp='vi $HOME/.bash_profile'
-alias br='vi $HOME/.bashrc'
-alias zr='vi $HOME/.zshrc'
-alias fr='vi $HOME/.config/fish/config.fish'
-alias vr='vi $HOME/.vimrc'
-alias gr='vi $HOME/.gitconfig'
-alias g='git'
-alias p='python'
-
-# dotfile copy alias
-alias cpdots='cp $HOME/.bash_profile $HOME/devapp/dotfiles && cp $HOME/.bashrc $HOME/devapp/dotfiles && cp $HOME/.zshrc $HOME/devapp/dotfiles && cp $HOME/.config/fish/config.fish $HOME/devapp/dotfiles && cp $HOME/.vimrc $HOME/devapp/dotfiles && cp $HOME/.gitconfig $HOME/devapp/dotfiles'
-
-# memo alias
-alias cdmemo='cd $HOME/devapp/memo/mymemo/ && ll && pwd && vsmemo'
-alias opmemo='code $HOME/devapp/memo'
-
-# Cordova alias
-alias chcordova 'anyenv -v && ndenv -v && node -v && npm -v && cordova -v'
-alias opcordova 'code $HOME/devapp/cordova'
-
-# SDK
-export PATH=$PATH:$HOME/Library/Android/sdk/tools
-export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
-
-# Homebrew
-export PATH=$PATH:/usr/local/sbin
-
-# maven
-# export PATH=$PATH:/usr/local/maven/bin
-
 # Java
 export JAVA_HOME=`/System/Library/Frameworks/JavaVM.framework/Versions/A/Commands/java_home -v "1.8"`
+PATH=${JAVA_HOME}/bin:${PATH}
 
-PATH=$JAVA_HOME/bin:$PATH
-
-export EDITOR=vi
+# alias
+alias br="vi .bashrc"
+alias ll="ls -l"
+alias la="ls -la"
+alias e="exit"
+alias d="docker"
+alias dm="docker-machine"
+alias dc="docker-compose"
+alias fr="vi $HOME/.config/fish/config.fish"
+alias raip="cd $HOME/dev/inc/raip2"
--- config.fish
+++ config.fish
@@ -1,67 +1,31 @@
-# Tatsuya's fish settings
-# Theme
-set fish_theme agnostr
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-
-# key bind
-function fish_user_key_bindings
-    # peco
-    bind \cr peco_select_history
-    # fzf
-    bind \ce __fzf_find_file
-    # ghq
-    bind \cg __ghq_crtl_g
-end
-
-# bobthefish
-# set -g theme_powerline_fonts yes
-# set -g theme_nerd_fonts yes
-
 # alias
 alias e 'exit'
-alias v 'vim'
-alias l 'll'
-alias lll 'l'
-alias ll 'ls -l'
-alias la 'ls -la'
-alias sfr 'source $HOME/.config/fish/config.fish'
-alias resbp 'exit; and reloadbp; and fish'
-alias bp 'vi $HOME/.bash_profile'
-alias br 'vi $HOME/.bashrc'
-alias zr 'vi $HOME/.zshrc'
-alias fr 'vi $HOME/.config/fish/config.fish'
-alias vr 'vi $HOME/.vimrc'
-alias gr 'vi $HOME/.gitconfig'
 alias g 'git'
-alias p 'pipenv run python'
+alias d 'docker'
+alias dm 'docker-machine'
+alias dme 'eval (docker-machine env)'
+alias dc 'docker-compose'
+alias fr 'vi $HOME/.config/fish/config.fish'
+alias memo 'code $HOME/dev/work/memo'
+alias c 'sudo purge'
 
-# dotfile copy alias
-alias cpdots 'cp $HOME/.bash_profile $HOME/devapp/dotfiles; and cp $HOME/.bashrc $HOME/devapp/dotfiles; and cp $HOME/.zshrc $HOME/devapp/dotfiles; and cp $HOME/.config/fish/config.fish $HOME/devapp/dotfiles; and cp $HOME/.vimrc $HOME/devapp/dotfiles; and cp $HOME/.gitconfig $HOME/devapp/dotfiles'
+# inc
+alias raip 'cd $HOME/dev/inc/raip2'
+alias doyo 'dm start; and dme; and raip; and cd env/; and dc up -d; and cd ../views; and yarn dev'
 
-# memo alias
-alias cdmemo 'cd $HOME/devapp/memo/mymemo/; and ll; and pwd'
-alias memo 'code $HOME/devapp/memo'
+# fish key bind
+function fish_user_key_bindings
+  # peco
+  bind \cr peco_select_history
+  # fzf
+  bind \ce __fzf_find_file
+end
 
-# Cordova alias
-alias chcordova 'anyenv -v; and ndenv -v; and node -v; and npm -v; and cordova -v; and adb version; and android -h'
-alias opcordova 'code $HOME/devapp/cordova'
-alias cdcordova 'cd $HOME/devapp/cordova; and ll; and pwd'
+# cd > ll
+function cd
+  builtin cd $argv
+  ll
+end
 
-# HelloCordova alias
-alias hc 'code $HOME/devapp/cordova/helloCordova'
-
-# SDK setup
-set PATH $HOME/Library/Android/sdk/tools $PATH
-set PATH $HOME/Library/Android/sdk/platform-tools $PATH
-
-# Homebrew
-# set PATH /usr/local/sbin $PATH
-
-# anyenv
-# set -x PATH $HOME/.anyenv/bin $PATH
-
-# yarn
-# set PATH /usr/local/bin/yarn $PATH
-
-# rbenv
-set PATH $HOME/.anyenv/envs/rbenv/bin $PATH
+# dotsfile
+alias cpdots 'cp $HOME/.bash_profile $HOME/dev/work/dotfiles; and cp $HOME/.bashrc $HOME/dev/work/dotfiles; and cp $HOME/.config/fish/config.fish $HOME/dev/work/dotfiles; and cp $HOME/.vimrc $HOME/dev/work/dotfiles; and cp $HOME/.gitconfig $HOME/dev/work/dotfiles'
